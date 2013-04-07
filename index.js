27cff1d11a8259deb82c93fe50d55ce09cb13809
judnich <electrograv@gmail.com>
2013-04-07 02:16:34 -0700

    Reorganized javascript/shaders into separate files

---
+++ index.js
@@ -0,0 +1,129 @@
+
+_speed = 0.0;
+_reverseMode = false;
+_sliderMouseDown = false;
+
+function jsMain() {
+	function resizeCanvas() {
+		// correct canvas dimensions due to sidebar
+		document.getElementById("rightbar").style.width = 
+			(window.innerWidth - document.getElementById("sidebar").clientWidth) + "px";
+	}
+	resizeCanvas();
+
+	// set up slider events
+	var slider = document.getElementById("slider");
+
+	function sliderEvent(x, y) {
+		_speed = 1.0 - (y / (slider.clientHeight+3));
+		if (_speed > 1) _speed = 1;
+		updateSpeed();
+	}
+
+	slider.addEventListener("mousedown", function(event) {
+		_sliderMouseDown = true;
+		sliderEvent(event.offsetX, event.offsetY);
+	}, false);
+	slider.addEventListener("mouseup", function(event) {
+		_sliderMouseDown = false;
+	}, false);
+	slider.addEventListener("mousemove", function(event) {
+		if (_sliderMouseDown) {
+			sliderEvent(event.offsetX, event.offsetY);
+		}
+	}, false);
+	document.addEventListener("mouseup", function(event) {
+		_sliderMouseDown = false;
+	}, false);
+
+
+	// CoffeeScript code entry point
+	kosmosMain();
+
+	updateSpeed();
+	showIntro();
+
+	window.onresize = function() {
+		resizeCanvas();
+		kosmosResize();
+		updateIntro();
+	}
+
+	document.onkeydown = function(event) {
+		hideIntro();
+		if (event.keyCode == 189 || event.keyCode == 83 || event.keyCode == 40) {
+			_speed -= 0.025;
+		}
+		if (event.keyCode == 187 || event.keyCode == 87 || event.keyCode == 38) {
+			_speed += 0.025;
+		}
+		if (_speed > 1.5) _speed = 1.5;
+
+		if (event.keyCode == 27) { // escape
+			killSimulation();
+		}
+		updateSpeed();
+	}
+}
+
+function hideIntro() {
+	var msg = document.getElementById("introMessage");
+	msg.style.display = "none";
+	//msg.parentNode.removeChild(msg);
+}
+
+function updateIntro() {
+	var msg = document.getElementById("introMessage");
+	var sidebarWidth = document.getElementById("sidebar").clientWidth;
+	var x = sidebarWidth + (window.innerWidth-sidebarWidth)/2 - (msg.clientWidth+23)/2;
+	var y = window.innerHeight/2 - (msg.clientHeight+23)/2;
+	msg.style.left = x;
+	msg.style.top = y;
+}
+
+function showIntro() {
+	var msg = document.getElementById("introMessage");
+	msg.style.display = "block";
+	updateIntro();
+}
+
+function updateSpeed() {
+	hideIntro();
+
+	// the first few percent of the speed bar is 0, to give a reasonable click region for it
+	var speedBarZeroArea = 0.07;
+	var slider = document.getElementById("slider");
+	if (_speed < speedBarZeroArea) {
+		_speed = speedBarZeroArea;
+		slider.className = "slider sliderDisabled";
+	} else {
+		slider.className = "slider";
+	}
+
+	var cSpeed = _speed;
+	if (cSpeed > 1) cSpeed = 1; else if (cSpeed < 0) cSpeed = 0;
+	var sliderBar = document.getElementById("sliderBar");
+	sliderBar.style.height = ((1.0 - cSpeed) * 100.0) + "%";
+
+	var speed = (_speed - speedBarZeroArea);
+
+	var espeed = Math.pow(3.0, Math.abs(speed) * 20.0 - 10.0);
+	if (_reverseMode) espeed *= -1;
+
+	kosmosSetSpeed(espeed);
+}
+
+function updateButtons() {
+	document.getElementById("reverse").className = (_reverseMode ? "btn lit" : "btn");
+	//document.getElementById("stop").className = (_stoppedMode ? "btn redlit" : "btn red");
+}
+
+function toggleReverse() {
+	_reverseMode = !_reverseMode;
+	updateButtons();
+	updateSpeed();
+}
+
+function killSimulation() {
+	kosmosKill();
+}
---
+++ shaders/StarfieldShader.coffee
@@ -0,0 +1,72 @@
+
+frag = """
+
+precision mediump float;
+
+varying vec3 vUVA;
+varying vec3 vColor;
+
+void main(void) {
+	// compute star color based on intensity = 1/dist^2 from center of sprite
+	vec2 dv = vUVA.xy - vec2(0.5, 0.5);
+	float d = dot(dv, dv);
+	float lum = 1.0 / (d*100.0);
+	//float c = 1.0 / (d*10.0);
+	//c = clamp(0.5 - d, 0.0, 1.0) * c;
+
+	vec4 pColor = vec4(clamp(lum*vColor.x, 0.0, 1.0), clamp(lum*vColor.y, 0.0, 1.0), clamp(lum*vColor.z, 0.0, 1.0), 1.0);
+    gl_FragColor = pColor * vUVA.z;
+}
+
+"""
+
+
+vert = """
+
+attribute vec4 aPos;
+attribute vec2 aUV;
+
+uniform mat4 projMat;
+uniform mat4 modelViewMat;
+uniform vec2 starSizeAndViewRange;
+//uniform mat4 modelMat;
+//uniform mat4 viewMat;
+
+varying vec3 vUVA;
+varying vec3 vColor;
+
+void main(void) {
+	// determine star size
+	float starSize = starSizeAndViewRange.x;
+	//starSize = starSize * (cos(aPos.w*1000.0) * 0.5 + 1.0); // modulate size by simple PRNG
+
+	// compute vertex position so quad is always camera-facing
+	vec4 pos = vec4(aPos.xyz, 1.0);
+	vec2 offset = (aUV - 0.5) * starSize;
+
+	//pos = viewMat * modelMat * pos;
+	pos = modelViewMat * pos;
+	pos.xy += offset;
+
+	// fade out distant stars
+	float dist = length(pos.xyz);
+	float alpha = clamp((1.0 - (dist / starSizeAndViewRange.y)) * 4.0, 0.0, 1.0);
+
+    // the UV coordinates are used to render the actual star radial gradient,
+    // and alpha is used to modulate intensity of distant stars as they fade out
+    vUVA = vec3(aUV, alpha);
+
+    // compute star color parameter
+    // this is just an arbitrary hand-tweaked interpolation between blue/white/red
+    // favoring mostly blue and white with some red
+    vColor = vec3(1.0 - aPos.w, aPos.w*2.0*(1.0-aPos.w), 2.0 * aPos.w) * 0.5 + 0.5;
+
+	// output position, or degenerate triangle if star is beyond view range
+	if (alpha > 0.0)
+    	gl_Position = projMat * pos;
+    else
+    	gl_Position = vec4(0, 0, 0, 0);
+}
+"""
+
+xgl.addProgram("starfield", vert, frag)
